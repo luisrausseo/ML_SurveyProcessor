e5272dbdf98691fba165182f4b1363ef8556618a
Luis Rausseo <luisrausseo22@gmail.com>
2019-03-02 16:57:56 -0600

    Update

--- linearCSV.py
+++ linearCSV.py
@@ -43,14 +43,13 @@ class SurveyAgent:
 		conn = pymssql.connect(server, user, password, dataBase)
 		cursor = conn.cursor()
 		
-		sql = "";
+		sql = """SELECT CONCAT('__label__', label) as label, LTRIM(SUBSTRING([text], CHARINDEX('>', [text])+5, 1000)) as text FROM ( 
+		SELECT LTRIM(RTRIM(SUBSTRING([mrALLDESCRIPTIONS], 0, CHARINDEX('ORIGINAL ISSUE =', [mrALLDESCRIPTIONS])))) as text 
+		,[Customer__bComments__bRating] as label 
+		FROM [Footprints].[dbo].[MASTER14] 
+		WHERE NOT [Customer__bComments__bRating] IN (0, '__u1')) a""";
 		
-		cursor.execute("SELECT CONCAT('__label__', label) as label, LTRIM(SUBSTRING([text], CHARINDEX('>', [text])+5, 1000)) as text FROM (
-	  SELECT LTRIM(RTRIM(SUBSTRING([mrALLDESCRIPTIONS], 0, CHARINDEX('ORIGINAL ISSUE =', [mrALLDESCRIPTIONS])))) as text 
-	  ,[Customer__bComments__bRating] as label
-	  FROM [Footprints].[dbo].[MASTER14] 
-	  WHERE NOT [Customer__bComments__bRating] IN (0, '__u1')
-	 ) a'")
+		cursor.execute(sql)
 		
 		conn.close()
 
